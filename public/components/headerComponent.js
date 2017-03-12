e745e0611c80806436e5bc6f7bd9f7b9d3e5dff5
elliot richardson <ear78@aol.com>
2017-03-11 19:59:07 -0600

    finished controller for mobile sidemenu

--- public/components/headerComponent.js
+++ public/components/headerComponent.js
@@ -1,4 +1,17 @@
 angular.module('myApp')
     .component('headerComponent', {
         templateUrl: '../views/components/headerComponent.html',
+        controller: function headerCtrl(){
+            var self = this;
+
+            // SHOW HIDE MODAL BACKGROUND
+            self.toggleMenu = true;
+            // TOGGLE CLASS FOR MENU ANIMATION
+            self.isActive = false;
+
+            self.openMenu = function(){
+                self.toggleMenu = !self.toggleMenu;
+                self.isActive = !self.isActive;
+            }
+        }
     })
